cf113e0e2f1a8676485cdfaf40dd4219f00fb4d1
CloudEdge Solutions <rob_hirschfeld@dell.com>
2011-09-11 19:55:28 -0500

    move the barclamp installation scripts into the crowbar barclamp.  this creates a bootstrapping challenge that had to be addressed in the common_install.sh script and by having the requires look in 2 places.  Ultimately, this makes it practical to upgrade the barclamp install scripts.

---
+++ bin/barclamp_uninstall.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby
+# Copyright 2011, Dell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: RobHirschfeld
+#
+
+  # the 1st choice is to use the code from the framework since it is most up to date
+  # however, that code is not always available when installing
+  fw_lib = File.join '/opt', 'dell', 'crowbar_framework', 'lib', 'barclamp_lib'
+  if File.exists? fw_lib+'.rb'
+    require fw_lib
+  else
+    require File.join '/opt', 'dell', 'barclamps', 'crowbar', 'crowbar_framework', 'lib', 'barclamp_lib'
+  end
+    
+  # this is used by the install-chef installer script 
+  if __FILE__ == $0
+    path = ARGV[0]
+    puts "Using #{path}" if DEBUG
+    barclamp = YAML.load_file File.join path, 'crowbar.yml'
+    bc = barclamp["barclamp"]["name"].chomp.strip
+    case barclamp["crowbar"]["layout"].to_i
+    when 1
+      bc_remove_layout_1 bc, path, barclamp
+      #TODO: bc_remove_layout_1_chef bc, path, barclamp
+    else
+      puts "ERROR: could not UNinstall barclamp #{bc} because #{barclamp["barclamp"]["crowbar_layout"]} is unknown layout."
+      exit -3
+    end
+    #exit -3 if err
+    exit 0
+  end
+  

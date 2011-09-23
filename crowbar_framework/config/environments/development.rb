21f42b36ea71b8fb0c96645e77afd19f488c09c8
Rob @Zehicle Hirschfeld <rob_hirschfeld@dell.com>
2011-09-23 00:03:41 -0500

    fix dev config so that it can run on the admin server when rainbow is run in dev mode

--- crowbar_framework/config/environments/development.rb
+++ crowbar_framework/config/environments/development.rb
@@ -32,11 +32,8 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-CHEF_SERVER_URL = "http://localhost:4000"
-CHEF_CLIENT_KEY = "tmp\\chefkey.pem"
-CHEF_NODE_NAME = "WN7-J84NHL1"
-CHEF_ONLINE = true
+CHEF_CLIENT_KEY = "/opt/dell/crowbar_framework/config/client.pem"
 CROWBAR_VERSION = "Development"
-CHEF_ONLINE = true
+CHEF_ONLINE = File.exist? CHEF_CLIENT_KEY
 OFFLINE_FILES_DIR = 'db'
 

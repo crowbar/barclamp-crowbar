5c8467b07c01c0829d75e4dda3786fa5b3cb4ded
Rob @Zehicle Hirschfeld <rob_hirschfeld@dell.com>
2011-09-30 11:24:15 -0500

    make sass add more robust and allow additions at top or bottom (default) of list.

--- crowbar_framework/config/navigation.rb
+++ crowbar_framework/config/navigation.rb
@@ -4,12 +4,14 @@ SimpleNavigation::Configuration.run do |navigation|
       secondary.item :dashboard, t('nav.dashboard'), root_path
       secondary.item :dashboard, t('nav.list'), nodes_list_path
     end
-    primary.item :barclamps, 'NEW-'+t('nav.barclamps'), barclamp_modules_path do |secondary|
+    primary.item :barclamps, t('nav.barclamps')+ ' v1.1', barclamp_modules_path do |secondary|
       # barclamps can add menu items here
     end
-    primary.item :barclamps, t('nav.barclamps'), barclamp_index_barclamp_path
-    primary.item :proposals, t('nav.proposals'), barclamp_proposals_barclamp_path
-    primary.item :roles, t('nav.roles'), barclamp_roles_barclamp_path
+    unless RAILS_ENV=='development'
+      primary.item :barclamps, t('nav.barclamps'), barclamp_index_barclamp_path
+      primary.item :proposals, t('nav.proposals'), barclamp_proposals_barclamp_path
+      primary.item :roles, t('nav.roles'), barclamp_roles_barclamp_path
+    end
     primary.item :help, t('nav.help'), '/users_guide.pdf', { :link => { :target => "_blank" } }
   end
 end

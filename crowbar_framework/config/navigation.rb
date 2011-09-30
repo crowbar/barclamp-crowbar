d0ff88cd9297fe746860daf24c2e32c8fb6f8619
Rob @Zehicle Hirschfeld <rob_hirschfeld@dell.com>
2011-09-30 12:07:36 -0500

    cleanups for UI

--- crowbar_framework/config/navigation.rb
+++ crowbar_framework/config/navigation.rb
@@ -2,7 +2,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :nodes, t('nav.nodes'), root_path do |secondary|
       secondary.item :dashboard, t('nav.dashboard'), root_path
-      secondary.item :dashboard, t('nav.list'), nodes_list_path
+      secondary.item :dashboard, t('nav.list')+' v1.1', nodes_list_path
     end
     primary.item :barclamps, t('nav.barclamps')+ ' v1.1', barclamp_modules_path do |secondary|
       # barclamps can add menu items here

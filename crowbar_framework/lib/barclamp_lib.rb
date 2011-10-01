5af9c36342f810af05e658eeea00dc4ad8e52a2f
Greg Althaus <galthaus@austin.rr.com>
2011-09-30 22:20:12 -0500

    Make sure file are in place before we edit them.

--- crowbar_framework/lib/barclamp_lib.rb
+++ crowbar_framework/lib/barclamp_lib.rb
@@ -273,17 +273,18 @@
     
     puts "Installing barclamp #{bc} from #{path}"
 
-    #merge i18n information (least invasive operations first)
-    merge_i18n barclamp
-    merge_nav barclamp, true
-    merge_sass barclamp, bc, path, true
-
     #copy the rails parts (required for render BEFORE import into chef)
     dirs = Dir.entries(path)
     if dirs.include? 'crowbar_framework'
       files += bc_cloner('crowbar_framework', bc, nil, path, BASE_PATH, false) 
       framework_permissions bc, path
     end
+
+    #merge i18n information (least invasive operations first)
+    merge_i18n barclamp
+    merge_nav barclamp, true
+    merge_sass barclamp, bc, path, true
+
     if dirs.include? 'bin'
       files += bc_cloner('bin', bc, nil, path, BASE_PATH, false) 
       FileUtils.chmod_R 0755, BIN_PATH

32ddc21c4314ea76cdbbe70920090f044a8b47a1
Rob @Zehicle Hirschfeld <rob_hirschfeld@dell.com>
2011-10-14 17:26:31 -0500

    Change menus based on QA request to hide new forms New menus are available in DevMode Will be added as defaults in 2 weeks.

--- crowbar_framework/config/navigation.rb
+++ crowbar_framework/config/navigation.rb
@@ -1,10 +1,11 @@
 SimpleNavigation::Configuration.run do |navigation|  
   navigation.items do |primary|
     primary.item :nodes, t('nav.nodes'), root_path do |secondary|
-      secondary.item :dashboard, t('nav.dashboard'), root_path
-      secondary.item :dashboard, t('nav.list'), nodes_list_path
+      secondary.item :dashboard, t('nav.dashboard'), root_path if RAILS_ENV == 'development'
+      secondary.item :dashboard, t('nav.list'), nodes_list_path if RAILS_ENV == 'development'
     end
     primary.item :barclamps, t('nav.barclamps'), barclamp_modules_path do |secondary|
+      secondary.item :barclamps, t('nav.all_bc'), barclamp_modules_path if RAILS_ENV == 'development'
       # barclamps can add menu items here
     end
     primary.item :help, t('nav.help'), '/users_guide.pdf', { :link => { :target => "_blank" } }

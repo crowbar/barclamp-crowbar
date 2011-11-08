8766a38cefbd0b62c360d705126bdcc63e096b2a
Victor Lowther <victor_lowther@dell.com>
2011-11-08 13:38:18 -0600

    Update kernel-panic recipe to allow disabling reboot on panic.

--- chef/cookbooks/kernel-panic/recipes/default.rb
+++ chef/cookbooks/kernel-panic/recipes/default.rb
@@ -8,17 +8,29 @@ if node["panic"] and node["panic"]["reboot"] == true
   if ::File.exists?("/etc/sysctl.d") and ::File.directory?("/etc/sysctl.d")
     bash "Save reboot on panic" do
       code "echo 'kernel.panic = #{timeout}' >/etc/sysctl.d/80-reboot-on-panic.conf"
-      not_if "test -f /etc/sysctl.d/80-reboot-on-panic.conf"
     end
-  elsif ::File.exists?("/etc/sysctl.conf")
+  else
     bash "Save reboot on panic" do
-      code "echo 'kernel.panic = #{timeout}' >>/etc/sysctl.conf"
-      not_if "grep -q 'kernel.panic' /etc/sysctl.conf"
+      code <<-__EOC__
+if grep -q 'kernel.panic' /etc/sysctl.conf; then
+sed -i '/^kernel.panic/ s/(kernel.panic = ).*/\1#{timeout}/' /etc/sysctl.conf
+else
+echo 'kernel.panic = #{timeout}' >>/etc/sysctl.conf
+fi
+__EOC__
     end
-  end
-  
+  end 
   bash "Reboot on panic" do
     code "echo #{timeout} >/proc/sys/kernel/panic"
-    only_if "test -f /proc/sys/kernel/panic"
+  end
+else
+  bash "Stop rebooting on panic" do
+    code <<-__EOC__
+if [[ -f /etc/sysctl.d/80-reboot-on-panic.conf ]]; then
+rm -f /etc/sysctl.d/80-reboot-on-panic.conf
+fi
+sed -i '/kernel.panic/ s/.*//' /etc/sysctl.conf
+echo 0 >/proc/sys/kernel/panic
+__EOC__
   end
 end

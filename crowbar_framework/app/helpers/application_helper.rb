958c14924f308dc7fb5154aaa99f293eb45898c0
Jim <James_Clement@dell.com>
2011-11-29 12:09:16 -0500

    DE470: Barclamp Proposals Attribute Page munges attributes together
    3 Changes:
    1. Close <li> tag in all circumstances, not just if attrib value is ! a Hash
    2. Only display the colon between Attrib names and their values if the attrib *has* a value
    3. Handle Attrib keys which are themselves hashes by recursing.
    
    CR done by Rob
    
    Signed-off-by: Jim <James_Clement@dell.com>

--- crowbar_framework/app/helpers/application_helper.rb
+++ crowbar_framework/app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
-# Copyright 2011, Dell 
-# 
-# Licensed under the Apache License, Version 2.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
-# 
-#  http://www.apache.org/licenses/LICENSE-2.0 
-# 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
-# limitations under the License. 
-# 
-# Author: RobHirschfeld 
-# 
+# Copyright 2011, Dell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: RobHirschfeld
+#
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   # Is this a Rails 2-ism - csrf_meta_tag?
@@ -26,7 +26,7 @@ module ApplicationHelper
               Rack::Utils.escape_html(form_authenticity_token) ]
     end
   end
-  
+
   def dl_item(term, definition, options={})
     unless definition.blank? && options[:show_if_blank] != true
       html  = "<dt>#{options[:escape_html] != false ? (h term) : (term)}</dt>"
@@ -34,7 +34,7 @@ module ApplicationHelper
       raw html
     end
   end
-  
+
   def column_class(current_column, total)
     if (current_column % total) == 0
       "first"
@@ -44,16 +44,22 @@ module ApplicationHelper
   end
 
   def hash_to_ul(hash)
-    result = "<ul>"
-    hash.each do |key,value|
-      result << "<li>#{key}"
-      if value.is_a?(Hash)
-        result << hash_to_ul(value)
-      else
-        result << ": #{value}</li>"
+      result = "<ul>"
+      hash.each do |key,value|
+          result << "<li>"
+          if key.is_a?(Hash)
+              result << hash_to_ul(key)
+          else
+              result << "<em>#{key}</em>"
+          end
+          if value.is_a?(Hash)
+              result << hash_to_ul(value)
+          else
+              result << ( value == nil ? "" : ": #{value}" )
+          end
+          result << "</li>"
       end
-    end
-    result << "</ul>"
+      result << "</ul>"
   end
 
   def render_attributes(raw, proposal)
@@ -80,7 +86,7 @@ module ApplicationHelper
         render :partial => "barclamp/#{proposal.barclamp}/edit_deployment"
       rescue ActionView::MissingTemplate
         render :partial => 'barclamp/edit_deployment_raw'
-      rescue 
+      rescue
         puts "Deployment Exception #{e.message}"
         puts e.backtrace
         render :partial => 'barclamp/edit_deployment_raw'

7e5f87631117211edbd99c1f4baeb3e840021776
Rob @Zehicle Hirschfeld <rob_hirschfeld@dell.com>
2012-01-11 19:42:47 -0600

    more null protections for systems in discovering state

--- crowbar_framework/app/controllers/nodes_controller.rb
+++ crowbar_framework/app/controllers/nodes_controller.rb
@@ -41,7 +41,7 @@ class NodesController < ApplicationController
         group = node.group
         @groups[group] = { :automatic=>!node.display_set?('group'), :status=>{"ready"=>0, "failed"=>0, "unknown"=>0, "unready"=>0, "pending"=>0}, :nodes=>{} } unless @groups.key? group
         @groups[group][:nodes][node.group_order] = node.handle
-        @groups[group][:status][node.status] = @groups[group][:status][node.status].to_i + 1
+        @groups[group][:status][node.status] = (@groups[group][:status][node.status] || 0).to_i + 1
         if node.handle === params[:name]
           @node = node
           get_node_and_network(node.handle)

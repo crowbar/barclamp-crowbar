96eeeda0862c0a3b6dea3c6adb5271789103b993
Ralf Haferkamp <rhafer@suse.de>
2012-04-23 16:22:48 +0200

    Add SUSE specific mappings for the apache2 service

--- chef/cookbooks/apache2/recipes/default.rb
+++ chef/cookbooks/apache2/recipes/default.rb
@@ -38,6 +38,12 @@ service "apache2" do
     # during the initial bootstrap.
     restart_command "/sbin/service httpd restart && sleep 1"
     reload_command "/sbin/service httpd graceful && sleep 1"
+  when "suse"
+    # If restarted/reloaded too quickly httpd has a habit of failing.
+    # This may happen with multiple recipes notifying apache to restart - like
+    # during the initial bootstrap.
+    restart_command "/sbin/service httpd restart && sleep 1"
+    reload_command "/sbin/service httpd graceful && sleep 1"
   when "debian"
     service_name "apache2"
     restart_command "/usr/sbin/invoke-rc.d apache2 restart && sleep 1"
@@ -55,6 +61,7 @@ service "apache2" do
     "centos" => { "default" => [ :restart, :reload, :status ] },
     "redhat" => { "default" => [ :restart, :reload, :status ] },
     "fedora" => { "default" => [ :restart, :reload, :status ] },
+    "suse" => { "default" => [ :restart, :reload, :status ] },
     "arch" => { "default" => [ :restart, :reload, :status ] },
     "default" => { "default" => [:restart, :reload ] }
   )

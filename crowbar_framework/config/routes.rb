11bee205c97b35af48a1ded3290cad77e2bbd28a
Rob @Zehicle Hirschfeld <rob_hirschfeld@dell.com>
2012-04-25 11:46:42 -0500

    Changing from export menu items to buttons.  Code will search all controllers for "export" method and provide buttons if there is an export method. If this works, we will remove the menu items for Chef & Logging export. If you like this, thank Randy Perryman.

--- crowbar_framework/config/routes.rb
+++ crowbar_framework/config/routes.rb
@@ -43,6 +43,7 @@ ActionController::Routing::Routes.draw do |map|
   map.utils 'utils', :controller=>'support', :action=>'index'
   map.utils_files 'utils/files/:id', :controller=>'support', :action=>'index', :requirements => { :id => /.*/ }
   map.export_chef 'utils/chef', :controller=>'support', :action=>'export_chef'
+  map.utils_export 'utils/:controller/1.0/export', :action=>'export'
   map.utils_barclamp 'utils/:controller/1.0', :action=>'utils'
   map.utils_import 'utils/import/:id', :controller=>'support', :action=>'import', :requirements => { :id => /.*/ }
   map.utils_upload 'utils/upload/:id', :controller=>'support', :action=>'upload', :requirements => { :id => /.*/ }

1d8142033a53893eb1ac35ae3a023dd516ad3145
Ralf Haferkamp <rhafer@suse.de>
2012-05-03 11:19:57 +0200

    Fix service names on SUSE

--- chef/cookbooks/apache2/recipes/default.rb
+++ chef/cookbooks/apache2/recipes/default.rb
@@ -39,11 +39,9 @@ service "apache2" do
     restart_command "/sbin/service httpd restart && sleep 1"
     reload_command "/sbin/service httpd graceful && sleep 1"
   when "suse"
-    # If restarted/reloaded too quickly httpd has a habit of failing.
-    # This may happen with multiple recipes notifying apache to restart - like
-    # during the initial bootstrap.
-    restart_command "/sbin/service httpd restart && sleep 1"
-    reload_command "/sbin/service httpd graceful && sleep 1"
+    service_name "apache2"
+    restart_command "/sbin/service apache2 restart && sleep 1"
+    reload_command "/sbin/service apache2 graceful && sleep 1"
   when "debian"
     service_name "apache2"
     restart_command "/usr/sbin/invoke-rc.d apache2 restart && sleep 1"

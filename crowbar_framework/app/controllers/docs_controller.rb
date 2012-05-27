3b797d8a727777b81e35bf29355f97f31ed8707a
cloudedge <rob_hirschfeld@dell.com>
2012-05-27 10:36:25 -0700

    topics now working with new model.  content dynamically generated

--- crowbar_framework/app/controllers/docs_controller.rb
+++ crowbar_framework/app/controllers/docs_controller.rb
@@ -18,6 +18,7 @@
 class DocsController < ApplicationController
     
   require 'yaml'
+  META = 'topic_meta_data'
   
   def index
     doc_yml = File.join RAILS_ROOT, 'config', 'docs.yml'
@@ -26,57 +27,66 @@ class DocsController < ApplicationController
       @index = YAML.load_file File.join('config', 'docs.yml')
     else #create yml
       @index = gen_doc_index docs_path
-      #@index = gen_barclamps({'root'=>{'title'=>t('docs.root')}}, File.join(RAILS_ROOT,'doc','default'), :default)
-      #xref @index   #find the cross references from and update the index
       File.open( doc_yml, 'w' ) { |out| YAML.dump( @index, out ) }
     end
   end  
 
   def topic
-    all = YAML.load_file File.join('config', 'docs.yml')
-    id = params[:id].split('+')
-    case id.length
-    when 1
-      @parent = nil
-      @topic = nil
-    when 2 
-      @parent = nil
-      @topic = all[id[1]]
-    when 3 
-      @parent = all[id[1]]
-      @topic = all[id[1]][id[2]]
-    when 4
-      @parent = all[id[1]][id[2]]
-      @topic = all[id[1]][id[2]][id[3]]
-    else
-      raise "documentation nested to too many levels, max is 7"
+    begin 
+      all = YAML.load_file File.join('config', 'docs.yml')
+      @path = params[:id]
+      id = @path.split('+')
+      case id.length
+      when 1
+        @parent = nil
+        @parent_link = nil
+        @topic = nil
+      when 2 
+        @parent = nil
+        @parent_link = nil
+        @topic = all[id[1]]
+      when 3 
+        @parent = all[id[1]]
+        @parent_link = id[0..1].join('+')
+        @topic = all[id[1]][id[2]]
+      when 4
+        @parent_link = id[0..2].join('+')
+        @parent = all[id[1]][id[2]]
+        @topic = all[id[1]][id[2]][id[3]]
+      when 5
+        @parent_link = id[0..3].join('+')
+        @parent = all[id[1]][id[2]][id[3]]
+        @topic = all[id[1]][id[2]][id[3]][id[4]]
+      when 6
+        @parent_link = id[0..4].join('+')
+        @parent = all[id[1]][id[2]][id[3]][id[4]]
+        @topic = all[id[1]][id[2]][id[3]][id[4]][id[5]]
+      else
+        raise "documentation nested to too many levels, max is 6"
+      end
+          
+      @meta = @topic['topic_meta_data']
+      file = @meta['file']
+      @index = {}
+      # navigation items
+      @next = 'foo' #all[@topic['nexttopic']] if @topic['nexttopic']
+      @prev =  'foo' #@topic['prevtopic']
+      @children = @topic.delete_if { |k, v| k == META }
+      from = @meta['file']  
+      raw = if File.exist? file
+        %x[markdown #{from}]
+      else
+        I18n.t '.topic_missing', :scope=>'docs.topic'
+      end
+      #File.open(@topic['source'], 'r').each do |s|
+      #  raw += s
+      #end
+      #markdown = Redcarpet.new "raw", []
+      @text = raw #markdown.to_html
+    rescue
+      @text = I18n.t '.topic_missing', :scope=>'docs.topic'
+      flash[:notice] = @text
     end
-        
-    @meta = @topic['topic_meta_data']
-    file = @meta['file']
-    @index = {}
-    # navigation items
-    @next = 'foo' #all[@topic['nexttopic']] if @topic['nexttopic']
-    @prev =  'foo' #@topic['prevtopic']
-    #@prev.each { |p| @index[p] = all[p] } if @prev
-    #parent = @topic['parent']
-    #@parents = 0
-    #while parent and all[parent] do 
-    #  @parents += 1
-    #  @index[parent] = all[parent]
-    #  parent = all[parent]['parent']
-    #end
-    #if @topic['children']
-    #  @topic['children'].split(',').each do |t|
-    #    @index[t] = all[t]
-    #  end
-    #end
-    raw = "roo"
-    #File.open(@topic['source'], 'r').each do |s|
-    #  raw += s
-    #end
-    #markdown = Redcarpet.new "raw", []
-    @text = raw #markdown.to_html
   end
   
   private 
@@ -139,82 +149,6 @@ class DocsController < ApplicationController
         make_topics path, meta_data, barclamp, "#{parent}+#{id}", details.delete_if{ |k,v| k=='topic_meta_data' }
       end
     end
-  end
-
-
-  def gen_doc(meta, path, file)
-    key = file[/(.*).md$/,1]
-    meta['key']=key
-    doc_path = File.join 'public', 'doc', (meta['language']==:default ? 'en' : meta['language']), meta['barclamp']
-    html = File.join doc_path, file.gsub('.md','.html')
-    begin FileUtils.mkdir_p doc_path rescue true end
-    meta['url'] = "doc/#{meta['language']==:default ? 'en' : meta['language']}/#{meta['barclamp']}/#{file.gsub('.md','.html')}"
-    meta['source'] = File.join path, file
-    File.open(meta['source'], 'r').each do |s|
-      break if s.strip.length==0 
-      m = s.split(':')
-      if m.length>1 
-        meta_key = m[0][1..31].strip.downcase
-        case meta_key
-        when 'parent', 'nexttopic'
-          meta[meta_key] = m[1].strip.downcase
-        else
-          meta[meta_key] = m[1].strip
-        end 
-      end
-    end
-    meta['parent'] = 'root' unless meta['parent']  # we don't want any orphans!
-    meta['id'] = key # sometimes we get the object without the key
-    meta['format'] = 'markdown' if meta['format'].nil?
-    # from_type = meta['format'] || 'markdown+lhs'
-    # %x[pandoc -f #{from_type} -t html -o '#{html}' '#{File.join(path,file)}']
-    return meta
-  end
-  
-  def gen_barclamps(index, path, language)
-    Dir.entries(path).each do |bc|
-      index = gen_html(index, File.join(path, bc), language, bc) unless bc.start_with? '.'
-    end
-    index
-  end
-  
-  def gen_html(index, path, language, barclamp)
-    Dir.entries(path).each do |f|
-      if f =~ /.md$/
-        meta = gen_doc({ 'language'=>language, 'barclamp'=>barclamp }, path, f)
-        index["#{barclamp}\/#{meta['key']}"] = meta
-      end
-    end
-    index
-  end
-  
-  def xref(index)
-    index.each do |k, v|
-      if k and v
-        if v['parent'] and index[v['parent']]
-          unless index[v['parent']].key? 'children'
-            index[v['parent']]['children'] = k
-          else
-            # now sort the children
-            children = index[v['parent']]['children'].split(',')
-            children << k
-            child_order = {}
-            children.each do |child|
-              order = begin ("%06d" % index[child]['order'].to_i) rescue "009999"  end
-              child_order[child] = "#{order}-#{index[child]['title']}"
-            end
-            child_order = child_order.sort_by { |c, cd| cd }
-            child_list = child_order.map{|c ,cd| c}.join(',')
-            index[v['parent']]['children'] = child_list
-          end
-        end
-        if v['nexttopic'] and index[v['nexttopic']]
-          index[v['nexttopic']]['prevtopic'] = [] unless index[v['nexttopic']]['prevtopic']
-          index[v['nexttopic']]['prevtopic'] << k
-        end
-      end
-    end
-    index
   end
   
 end

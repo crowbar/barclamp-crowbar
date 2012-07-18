5124aee9c6dcfd8dfecab0d3382a037a77ad688e
Greg Althaus <galthaus@austin.rr.com>
2012-07-17 21:18:19 -0500

    Fix sass stylesheet generation.  (directory no longer needed).
    Clean up refs to stylesheets
    
    Remove csrf helper (not needed in rails3 - it has one)

--- crowbar_framework/app/helpers/application_helper.rb
+++ crowbar_framework/app/helpers/application_helper.rb
@@ -16,16 +16,6 @@
 #
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  # Is this a Rails 2-ism - csrf_meta_tag?
-  # app/helpers/application_helper.rb
-  def csrf_meta_tag
-    if protect_against_forgery?
-      out = %(<meta name="csrf-param" content="%s"/>\n)
-      out << %(<meta name="csrf-token" content="%s"/>)
-      out % [ Rack::Utils.escape_html(request_forgery_protection_token),
-              Rack::Utils.escape_html(form_authenticity_token) ]
-    end
-  end
 
   def dl_item(term, definition, options={})
     unless definition.blank? && options[:show_if_blank] != true

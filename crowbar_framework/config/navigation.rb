44403d72cde8ddd53fc6b870eddcc4d8a984c78b
Ralf Haferkamp <rhafer@suse.de>
2012-07-24 17:10:57 +0200

    Add items that were added by barclamp_install in the past
    We decided to not let barclamp_install change navigation.rb on the installed
    system, because it breaks upon rpm installing a newer version of the crowbar
    barclamp (see bnc#771839 for details).

--- crowbar_framework/config/navigation.rb
+++ crowbar_framework/config/navigation.rb
@@ -9,11 +9,13 @@ SimpleNavigation::Configuration.run do |navigation|
       # insert here for :nodes
     end
     primary.item :network, t('nav.network'), network_path do |secondary| 
+      secondary.item :vlan, t('nav.vlan'), vlan_path
       # insert here for :network
     end
     primary.item :barclamps, t('nav.barclamps'), barclamp_modules_path do |secondary|
       secondary.item :barclamps, t('nav.all_bc'), barclamp_modules_path
       secondary.item :crowbar, t('nav.crowbar_bc'), index_barclamp_path(:controller=>'crowbar')
+      secondary.item :openstack, t('nav.openstack'), index_barclamp_path(:controller=>'openstack')
       # insert here for :barclamps 
       # insert here for :add  (this is legacy support)
     end
@@ -21,12 +23,14 @@ SimpleNavigation::Configuration.run do |navigation|
       secondary.item :util_import, t('nav.util_import'), utils_import_path 
       secondary.item :util_index, t('nav.util_logs'), utils_path 
       secondary.item :util_chef, t('nav.util_chef'), export_chef_path 
+      secondary.item :log_export, t('nav.log_export'), utils_barclamp_path(:controller=>'logging')
       # insert here for :utils
     end
     primary.item :help, t('nav.help'), '/crowbar_users_guide.pdf', { :link => { :target => "_blank" } } do |secondary|
       secondary.item :help, t('nav.crowbar_wiki'), 'https://github.com/dellcloudedge/crowbar/wiki/', { :link => { :target => "_blank" } }
       secondary.item :help, t('nav.crowbar_ug'), '/crowbar_users_guide.pdf', { :link => { :target => "_blank" } }
       secondary.item :help, t('nav.crowbar_dg'), '/crowbar_deployment_guide.pdf', { :link => { :target => "_blank" } }
+      secondary.item :openstack, t('nav.openstack'), "/openstack_users_guide.pdf", { :link => { :target => "_blank" } }
       # insert here for :help 
     end
   end

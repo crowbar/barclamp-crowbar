15b5de771df39807afc3ce6eb1c6e1b507157566
Greg Althaus <galthaus@austin.rr.com>
2012-08-01 02:24:08 -0500

    Fix typo in name

--- crowbar_framework/app/controllers/scaffolds/barclamp_dependencies_controller.rb
+++ crowbar_framework/app/controllers/scaffolds/barclamp_dependencies_controller.rb
@@ -13,7 +13,7 @@
 # limitations under the License.
 #
 
-class Scaffolds::BarclampDependenciessController < ApplicationController
+class Scaffolds::BarclampDependenciesController < ApplicationController
   active_scaffold :barclamp_dependency do |conf|
   end
 end 

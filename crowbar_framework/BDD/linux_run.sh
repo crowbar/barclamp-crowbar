e93a6b36dd7d98a410b4bd5d88c04dcc252d050b
Greg Althaus <galthaus@austin.rr.com>
2012-08-20 23:28:39 -0500

    Start and stop puma based rails server

--- crowbar_framework/BDD/linux_run.sh
+++ crowbar_framework/BDD/linux_run.sh
@@ -1,2 +1,12 @@
+
+# Start rails
+cd ..
+script/rails s Puma &
+sleep 10
+cd BDD
+
 # Run Tests
 erl -s bdd test "crowbar" -s init stop -noshell
+
+# STop rails
+kill `ps -ef | grep Puma | grep ruby | awk '{ print $2 }'`

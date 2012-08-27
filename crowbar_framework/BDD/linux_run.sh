e873eec4842694f050476ebbbf6903b18e1c7d86
Greg Althaus <galthaus@austin.rr.com>
2012-08-27 16:27:43 -0500

    Rework the bdd linux runner a little.o

--- crowbar_framework/BDD/linux_run.sh
+++ crowbar_framework/BDD/linux_run.sh
@@ -1,12 +1,12 @@
 
 # Start rails
 cd ..
-script/rails s Puma &
+script/rails s Puma >> log/test.out 2>> log/test.err &
 sleep 10
 cd BDD
 
 # Run Tests
-erl -s bdd test "crowbar" -s init stop -noshell
+erl -s bdd test crowbar -s init stop -noshell
 
 # STop rails
 kill `ps -ef | grep Puma | grep ruby | awk '{ print $2 }'`

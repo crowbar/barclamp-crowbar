1374daee013766366c6ce2af9194ab8e44dfffd1
Greg Althaus <galthaus@austin.rr.com>
2012-10-20 18:41:47 -0500

    Add create_proposal unit tests for service_object and crowbar_service.
    Make rspec run under development (to use the database).
    Turn off the database cleaner code (but not the transaction code).
      - it is too aggressive on cleaning the database
      - The transactions seem to be sufficient
    
    Clean-up schema directory that has really old data.
    Make the code assume that the schemas are installed in the correct spot
    schema testing can now occur on import/create/update

--- crowbar_framework/app/models/proposal.rb
+++ crowbar_framework/app/models/proposal.rb
@@ -34,10 +34,12 @@
 # 
 
 class Proposal < ActiveRecord::Base
+
+  VALIDATION_EXPR = /^[a-zA-Z][_a-zA-Z0-9]*$/
   
   attr_accessible :name, :last_applied_rev, :description
 
-  validates_format_of :name, :with=>/^[a-zA-Z][_a-zA-Z0-9]*$/, :message => I18n.t("db.lettersnumbers", :default=>"Name limited to [_a-zA-Z0-9]")
+  validates_format_of :name, :with=>VALIDATION_EXPR, :message => I18n.t("db.lettersnumbers", :default=>"Name limited to [_a-zA-Z0-9]")
   validates_uniqueness_of :name, :scope => :barclamp_id, :case_sensitive => false, :message => I18n.t("db.notunique", :default=>"Name item must be unique")
 
   belongs_to :barclamp
--- crowbar_framework/spec/models/service_object_spec.rb
+++ crowbar_framework/spec/models/service_object_spec.rb
@@ -38,7 +38,8 @@ describe ServiceObject  do
   it "should allow bc_name to be set and update barclamp" do
     @service_object.bc_name = "crowbar"
     @service_object.bc_name.should eq("crowbar")
-    @service_object.barclamp.should be Barclamp.find_by_name("crowbar")
+    @service_object.barclamp.should be_an_instance_of Barclamp
+    @service_object.barclamp.name.should eq("crowbar")
   end
   
   describe "should have a password generator" do
@@ -62,8 +63,71 @@ describe ServiceObject  do
       answer[0].should be 200
       answer[1].should eq("")
     end
+  end
 
+  describe "Create Proposal" do
+    it "should fail with 400 and message if parms is nil" do
+      answer = @service_object.proposal_create(nil)
+      answer.should be_an_instance_of Array
+      answer[0].should be 400
+      answer[1].should eq(I18n.t('model.service.empty_parameters'))
+    end
+    it "should fail with 400 and message if parms is missing id" do
+      answer = @service_object.proposal_create({})
+      answer.should be_an_instance_of Array
+      answer[0].should be 400
+      answer[1].should eq(I18n.t('model.service.missing_id'))
+    end
+    it "should fail with 400 and message if proposal name is empty" do
+      answer = @service_object.proposal_create({"id" => ""})
+      answer.should be_an_instance_of Array
+      answer[0].should be 400
+      answer[1].should eq(I18n.t('model.service.too_short'))
+    end
+    def name_test_failure(name)
+      answer = @service_object.proposal_create({"id" => name})
+      answer.should be_an_instance_of Array
+      answer[0].should be 400
+      answer[1].should eq(I18n.t('model.service.illegal_chars', :name => name))
+    end
+    it "should fail with 400 and message if proposal name is illegal" do
+      name_test_failure("-=asg")
+      name_test_failure("s g")
+      name_test_failure("0012asdg")
+    end
+    it "should fail with exception if raw service_object is used" do
+      lambda {@service_object.proposal_create({"id" => "fred"})}.should raise_error(CrowbarException)
+    end
+    it "should fail with 400 and message if prop already exists" do
+      name = "fred"
+      cb = Barclamp.find_by_name("crowbar")
+      p = Proposal.new
+      p.name = name
+      p.barclamp = cb
+      p.save!
+
+      @service_object.bc_name = "crowbar"
+      answer = @service_object.proposal_create({"id" => name})
+      answer.should be_an_instance_of Array
+      answer[0].should be 400
+      answer[1].should eq(I18n.t('model.service.name_exists'))
+    end
+    it "should create a proposal with base data" do
+      ProposalObject.stub(:write) { [200, ""] }
+      @service_object.bc_name = "crowbar"
+      answer = @service_object.proposal_create({"id" => "fred"})
+      answer.should be_an_instance_of Array
+      answer[0].should be 200
+      answer[1].should eq("")
+      p = Proposal.find_by_name("fred")
+      p.name.should eq("fred")
+      p.barclamp.name.should eq("crowbar")
+      Proposal.find_all_by_barclamp_id(Barclamp.find_by_name("crowbar").id).length.should be 2
+    end
   end
 
 end
+
+
+
 

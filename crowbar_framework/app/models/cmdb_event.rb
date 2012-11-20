176f5c2da3ccf60a2dd6e6728c7191dee6eafed8
Judd Maltin <judd_maltin@dell.com>
2012-11-20 16:39:19 -0500

    very basics of CmdbEvent (and test class) with unit tests

--- crowbar_framework/app/models/cmdb_event.rb
+++ crowbar_framework/app/models/cmdb_event.rb
@@ -14,7 +14,10 @@
 #
 
 class CmdbEvent < ActiveRecord::Base
-  attr_accessible :name, :description, :order, :result, :status, :cmdb_run, :type
+  attr_accessible :name, :description, :type, :order, :result, :status, :cmdb_run_id
+
+  validates_uniqueness_of :name, :case_sensitive => false, :message => I18n.t("db.notunique", :default=>"Name item must be unique")
+  validates_format_of :name, :with=> /^[a-zA-Z][_a-zA-Z0-9]*$/, :message => I18n.t("db.lettersnumbers", :default=>"Name limited to [_a-zA-Z0-9]")
 
   # RESTORE THESE (after building tests)
   #belongs_to :cmdb_run
@@ -27,17 +30,19 @@ class CmdbEvent < ActiveRecord::Base
 
   # map attributes from cmdb node into array of CmdbAttributes
   def attrs_to_cmdb()
-    
+    puts "JWM placeholder"
   end
 
   def attrs_from_cmdb()
     #a = CmdbAttribute.new(:name => cmdb_attr, :value => node_attr_value)
     #a.save!
+    puts "JWM placeholder"
   end
   
   def run_cmdb_on_node()
-    
+    puts "JWM placeholder"
   end
+    
 
   # make sure I can get the map I need to put attrs in the DB
   def map(map_id)
@@ -58,7 +63,7 @@ class CmdbEvent < ActiveRecord::Base
      :order=> order,
      :result=> result,
      :status=> status,
-     :cmdb_run=> cmdb_run,
+     :cmdb_run_id=> cmdb_run_id,
      :type=> type,
      :created_at=> created_at,
      :updated_at=> updated_at

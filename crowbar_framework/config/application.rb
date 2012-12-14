24be6736e96182523597a4fbf0b03c2b733c1cd5
James Tan <jatan@suse.de>
2012-12-14 16:32:20 +0100

    Remove trailing whitespace

--- crowbar_framework/config/application.rb
+++ crowbar_framework/config/application.rb
@@ -58,33 +58,33 @@ module Crowbar
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  
+
     # Add additional load paths for your own custom dirs
     # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  
+
     # Specify gems that this application depends on and have them installed with rake gems:install
     # config.gem "bj"
     # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
     # config.gem "sqlite3-ruby", :lib => "sqlite3"
     # config.gem "aws-s3", :lib => "aws/s3"
-    
+
     config.gem "haml"
     config.gem "sass"
     config.gem "simple-navigation"
     config.gem "i18n"
     config.gem "json"
-    
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  
+
     # Activate observers that should always be running
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names.
     config.time_zone = 'UTC'
-  
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]

98c20e2d8850226700784ad2703b85065e6ec102
David Paterson <david_paterson@dell.com>
2013-01-07 20:20:45 -0800

    Checkpoint: basic network view controllers and network active scaffold config

--- crowbar_framework/config/navigation.rb
+++ crowbar_framework/config/navigation.rb
@@ -11,13 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-SimpleNavigation::Configuration.run do |navigation|  
 
+SimpleNavigation::Configuration.run do |navigation|    
+  puts "Build Navigationnnnnnnnnnnnnnnnnnn!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"   
   menu = Nav.find_by_item 'root'
   navigation.items do |primary|
-    menu.children.each do |item|
+    menu.children.each do |item| # Top Nav
       if item.item != 'root' and item.path =~ /(.*)_path/
         begin
+          puts "Build Navigation!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!item.path: #{item.path}"   
           name = t(item.name)
           title = t(item.description, :default=>t(item.name))
           if item.development
@@ -25,12 +27,17 @@ SimpleNavigation::Configuration.run do |navigation|
             title = "Dev Mode: "+title
           end
           primary.item item.item.to_sym, name, eval(item.path), {:title=>title} do |secondary|
+            puts "Build Navigation secondary #{secondary.inspect}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
             # make the top level help appear on the menu (for help only)
             if item.item.eql? 'help'
               Doc.find_by_name('root').children.each do |doc|
                 secondary.item doc.name.to_sym, t(doc.name, :scope=>'nav.books'), docs_path(:id=>doc.name.html_safe), {:title=>doc.description }
               end
             end
+            
+            if item.item.eql? 'scaffold'
+              build_scaffold_nav(secondary, item)
+            else
             # render menu items from the database
             item.children.each do |nav|
               if nav.path.starts_with? 'http'
@@ -39,12 +46,17 @@ SimpleNavigation::Configuration.run do |navigation|
                 if nav.development
                   secondary.item nav.item.to_sym, "[#{t(nav.name)}]", eval(nav.path), {:title=>"Dev Mode: #{t(nav.description, :default=>t(nav.name))}"} 
                 else
+                  puts "item.children.each nav: #{nav.inspect} wwwwwwwwwwwwwwwwwwwwwwwwww" 
                   secondary.item nav.item.to_sym, t(nav.name), eval(nav.path), {:title=>t(nav.description, :default=>t(nav.name))} 
+                  puts "after secondary.item secondary: #{secondary.inspect} rrrrrrr" 
                 end
               end 
             end
+            end
           end
-        rescue
+        rescue Exception => e  
+          puts "EXEPTION MESSAGE::::::::::::::::::::::::::::::::::::::::::::::: #{e.message}"   
+          puts "EXEPTION TRACE::::::::::::::::::::::::::::::::::::::::::::::: #{e.backtrace.inspect}" 
           primary.item :menu_error, "#{t 'nav.error'}: #{item.item}", ''
         end
       end
--- crowbar_framework/config/routes.rb
+++ crowbar_framework/config/routes.rb
@@ -25,7 +25,6 @@ Crowbar::Application.routes.draw do
     resources :proposal_configs do as_routes end
     resources :docs do as_routes end
     resources :navs do as_routes end
-    resources :groups do as_routes end  
     resources :interfaces do as_routes end
     resources :networks do as_routes end
     resources :ip_addresses do as_routes end
@@ -35,10 +34,35 @@ Crowbar::Application.routes.draw do
     resources :proposal_queues do as_routes end
     resources :proposal_queue_items do as_routes end
     resources :role_element_orders do as_routes end
-  end
+    resources :interface_maps do as_routes end
+    resources :bus_maps do as_routes end
+    resources :buses do as_routes end
+    resources :routers do as_routes end
+    resources :ip_ranges do as_routes end
+    resources :conduits do as_routes end
+    resources :conduit_rules do as_routes end
+    resources :conduit_filters do as_routes end
+    resources :node_attribute_filters do as_routes end
+    resources :network_mode_filters do as_routes end
+    resources :interface_selectors do as_routes end
+    resources :select_by_indices do as_routes end
+    resources :select_by_speeds do as_routes end
+    resources :conduit_actions do as_routes end
+    resources :create_bonds do as_routes end
+    resources :create_vlans do as_routes end
+ end
 
   resources :nodes, :only => [:index, :new] do
     get 'status', :on => :collection
+  end
+  
+  # resources :conduits
+  scope 'network' do
+    # resource :conduit
+    # resources :network, :only => [:show, :new, :create, :edit, :update, :destroy]
+    resources :networks, :conduits
+    # get 'network', :controller => 'network', :action=>'networks', :constraints => { :id => /.*/ }, :as => :network
+    # get 'conduit', :controller => 'conduit', :action=>'show', :constraints => { :id => /.*/ }, :as => :conduit
   end
 
 

a981ce62634621ee0aff61bb65f50c0cba016370
Rob Hirschfeld <rob_hirschfeld@dell.com>
2013-03-02 06:48:03 -0800

    add logic to deal w/ names that are not FQDN

--- crowbar_framework/db/migrate/20130228163840_create_admin_node.rb
+++ crowbar_framework/db/migrate/20130228163840_create_admin_node.rb
@@ -1,6 +1,8 @@
 class CreateAdminNode < ActiveRecord::Migration
-  def change
-    admin = Node.find_or_initialize_by_name(%x{hostname --fqdn}.strip)
+  def up
+    name = %x{hostname --fqdn}.strip
+    name += '.cr0wbar.com' unless name =~ Node::FQDN_RE
+    admin = Node.find_or_initialize_by_name(name)
     return if admin.persisted?
     admin.alias = admin.name
     admin.description = "Crowbar Admin Node"

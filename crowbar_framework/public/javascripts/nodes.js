9099821fe5d3c2914f181fc4df8a43c4a834098f
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-07-03 12:35:09 +0400

    typo

--- crowbar_framework/public/javascripts/nodes.js
+++ crowbar_framework/public/javascripts/nodes.js
@@ -1,5 +1,5 @@
 //
-// This JavaScript allow get values from nodes by name and keys
+// This JavaScript allow get value of attribute from nodes by name and attribute path
 //
 // Example:
 //   var public = node('admin').get('network/networks/public') -> {router: "192.168.122.1", netmask: "255.255.255.0", router_pref: 5, add_bridge: false, ...}

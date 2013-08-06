crowbar/barclamp-crowbar

commit f7f9c1a45f9d8bcf49012df8b030d745a9915fd7

Revert "Deactivate (and dequeue) proposal when deleting it"
This reverts commit a69f7417519febc110b84fac088d3a1cf05c7525.

Conflicts:

	crowbar_framework/app/models/service_object.rb

crowbar_framework/app/models/service_object.rb
@@ -558,8 +558,6 @@ class ServiceObject
   end
 
   def destroy_active(inst)
-    # Always dequeue to make sure it won't become active by accident later on
-    dequeue_proposal(inst)
 
     role_name = "#{@bc_name}-config-#{inst}"
     @logger.debug "Trying to deactivate role #{role_name}"
@@ -574,7 +572,7 @@ class ServiceObject
       dep[@bc_name]["config"].delete("crowbar-committing")
       dep[@bc_name]["config"].delete("crowbar-queued")
       role.override_attributes = dep
-      answer = apply_role(role, inst, false)
+      answer = apply_role(role, role_name, false)
       role.destroy
       answer
     end
@@ -649,9 +647,6 @@ class ServiceObject
   end
 
   def proposal_delete(inst)
-    # Deactivate the proposal first, in case it was in use
-    destroy_active(inst)
-
     prop = ProposalObject.find_proposal(@bc_name, inst)
     if prop.nil?
       [404, {}]

commit 7e264a86db502af12bba0fa0629f9860d45737b6

Fix destroy_active again to not use role name as instance name
The original fix was in c4acddcb, but it got broken by f7f9c1a4.

crowbar_framework/app/models/service_object.rb
@@ -587,7 +587,7 @@ class ServiceObject
       dep[@bc_name]["config"].delete("crowbar-committing")
       dep[@bc_name]["config"].delete("crowbar-queued")
       role.override_attributes = dep
-      answer = apply_role(role, role_name, false)
+      answer = apply_role(role, inst, false)
       role.destroy
       answer
     end
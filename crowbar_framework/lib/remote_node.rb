c0f4635711b26591a223055673d286b44ea43488
Sergey V. Yudin <tsipa740@gmail.com>
2013-08-29 10:56:53 +0000

    more logging

--- crowbar_framework/lib/remote_node.rb
+++ crowbar_framework/lib/remote_node.rb
@@ -9,14 +9,20 @@ module RemoteNode
     start = Time.now.to_i
     while (Time.now.to_i - start) < timeout do
       begin
+        puts "checking socket"
         TCPSocket.new(host, 22)
         if %w(redhat centos).include?(nobj[:platform])
+          puts "checking runlevel"
           raise "next" unless system("sudo -i -u root -- ssh root@#{host} 'runlevel | grep \"N 3\"'")
         else
+          puts "checking lastlog"
           raise "next" unless system("sudo -i -u root -- ssh root@#{host} 'last | head -n1 | grep -vq down'")
         end
         return true
-      rescue
+      rescue => e
+        puts "Next cycle due to:"
+        puts "#{e.inspect}"
+        puts "------------"
         sleep(10)
         next
       end

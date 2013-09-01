b7638d6ffdc056382563f7fd5278884021b5fccb
Rob Hirschfeld <rob_hirschfeld@dell.com>
2013-08-31 22:57:34 -0500

    turn off test timing delays (for annealer testing)

--- BDD/crowbar.erl
+++ BDD/crowbar.erl
@@ -92,12 +92,26 @@ step(_Global, {step_setup, _N, Test}) ->
   bdd_utils:alias(user, user_cb),
   bdd_utils:log(debug, crowbar, step, "Global Setup alias: ~p",[get({scenario,alias_map})]),
   bdd_utils:log(debug, crowbar, step, "Global Setup running (creating node ~p)",[g(node_name)]),
+  % turn off the delays in the test jig
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-admin", "property", "test", "to", "false"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-server", "property", "test", "to", "false"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-client", "property", "test", "to", "false"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-library", "property", "test", "to", "false"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-discovery", "property", "test", "to", "false"]}), 
+  % create node for testing
   Node = json([{name, g(node_name)}, {description, Test ++ g(description)}, {order, 100}, {admin, "true"}]),
   bdd_crud:create(node:g(path), Node, g(node_atom));
 
 % find the node from setup and remove it
 step(_Global, {step_teardown, _N, _}) -> 
   bdd_utils:log(debug, crowbar, step, "Global Teardown running",[]),
+  % turn on the delays in the test jig
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-admin", "property", "test", "to", "true"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-server", "property", "test", "to", "true"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-client", "property", "test", "to", "true"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-library", "property", "test", "to", "true"]}), 
+  role:step(Global, {step_given, {Scenario, _N}, ["I set the",role, "test-discovery", "property", "test", "to", "true"]}), 
+  % remove node for testing
   bdd_crud:delete(g(node_atom));
 
 % ============================  GIVEN STEPS =========================================
@@ -106,12 +120,13 @@ step(_Given, {step_when, _N, ["I18N checks",Key]}) ->
   URI = eurl:path(g(i18n),Key),
   eurl:get_http(URI);
 
-step(_Global, {step_given, {ScenarioID, _N}, ["there is a",role, Name]}) -> 
+step(Global, {step_given, {ScenarioID, _N}, ["there is a",role, Name]}) -> 
   bdd_utils:log(debug, crowbar, step, "REST creates the ~p ~p", [role, Name]),
   JSON = json([{name, Name}, {description, role:g(description)}, {order, role:g(order)}, {barclamp, "crowbar"}, {jig_name, "test"}]),
   Path = role:g(path),
   bdd_restrat:create(Path, JSON, role, ScenarioID);
 
+ 
 % ============================  WHEN STEPS =========================================
 
 step(_Given, {step_when, {Scenario, _N}, ["I add",node, Node,"to",deployment, Deployment,"in",role,Role]}) -> 

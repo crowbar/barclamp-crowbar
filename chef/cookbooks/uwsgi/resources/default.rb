cd7f7c8b0e4fc68335edf0dcbad1603a7ddf9287
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-11-20 16:26:20 +0400

    uwsgi support

---
+++ chef/cookbooks/uwsgi/resources/default.rb
@@ -0,0 +1,26 @@
+actions :enable, :disable
+
+attribute :name, :kind_of => String
+
+attribute :instances, :kind_of => [Hash, Array]
+attribute :options,  :kind_of => Hash
+attribute :service_name, :regex => /^[a-zA-Z0-9_-]+$/
+
+def initialize(name, run_context = nil)
+  super
+  set_platform_default_providers
+  @action = :enable
+end
+
+private
+def set_platform_default_providers
+  [:ubuntu, :debian].each do |platform|
+    Chef::Platform.set(
+      :platform => platform,
+      :resource => :uwsgi,
+      :provider => Chef::Provider::Uwsgi
+    )
+  end
+end
+
+

9d557ee7a9ee42059266bc0114b8179d2bf58413
Rob Hirschfeld <rob_hirschfeld@dell.com>
2013-11-25 00:47:36 -0600

    set simulator bootenv

--- BDD/crowbar.erl
+++ BDD/crowbar.erl
@@ -120,7 +120,7 @@ step(Global, {step_setup, {Scenario, _N}, Test}) ->
   network:make_admin(),
   % create node for testing
   bdd_utils:log(debug, crowbar, step, "Global Setup running (creating node ~p)",[g(node_name)]),
-  Node = json([{name, g(node_name)}, {description, Test ++ g(description)}, {order, 100}, {alive, "true"}, {admin, "true"}]),
+  Node = json([{name, g(node_name)}, {description, Test ++ g(description)}, {order, 100}, {alive, "true"}, {bootenv, node:g(bootenv)}, {admin, "true"}]),
   bdd_crud:create(node:g(path), Node, g(node_atom));
 
 % find the node from setup and remove it
@@ -137,7 +137,7 @@ step(_Given, {step_when, _N, ["I18N checks",Key]}) ->
 
 % we need to set alive value for new nodes
 step(_Given, {step_given, {ScenarioID, _N}, ["there is a",node,Name,"marked alive"]}) -> 
-  Node = json([{name, Name}, {description, g(description)}, {order, 200}, {alive, "true"}]),
+  Node = json([{name, Name}, {description, g(description)}, {order, 200}, {bootenv, node:g(bootenv)}, {alive, "true"}]),
   bdd_restrat:create(node:g(path), Node, node, ScenarioID);
 
 step(Global, {step_given, {ScenarioID, _N}, ["there is a",role, Name]}) -> 

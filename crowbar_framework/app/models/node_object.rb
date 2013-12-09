b5e7a6b823dfda8177b7a74f348f09cbce5c5427
Jiří Suchomel <jsuchome@suse.cz>
2013-12-09 10:34:33 +0100

    extending 'wait for chef' for other states

--- crowbar_framework/app/models/node_object.rb
+++ crowbar_framework/app/models/node_object.rb
@@ -940,14 +940,14 @@ class NodeObject < ChefObject
       save
     end
 
-    # wait with reboot for the finish of configuration update by local chef-client
-    # (so dhcp & PXE config is prepared when node is rebooted)
-    while state == "reinstall" && File.exist?("/var/run/crowbar/chef-client.lock")
-      Rails.logger.debug("chef client still running")
-      sleep(1)
-    end
+    if %w(reset reinstall update).include? state
+      # wait with reboot for the finish of configuration update by local chef-client
+      # (so dhcp & PXE config is prepared when node is rebooted)
+      while File.exist?("/var/run/crowbar/chef-client.lock")
+        Rails.logger.debug("chef client still running")
+        sleep(1)
+      end
 
-    if state == "reset" or state == "reinstall" or state == "update"
       if CHEF_ONLINE
         bmc_cmd("power cycle")
       else

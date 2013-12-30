f864139feadc9c9c1001e252b907162b999d5336
Balazs Kutil <bkutil@suse.com>
2013-12-30 17:41:02 +0100

    Add first spec

---
+++ crowbar_framework/spec/controllers/nodes_controller_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "spec_helper"))
+
+describe NodesController do
+  before do
+    RoleObject.stubs(:find_role_by_name).returns(RoleObject.new(Chef::Role.new))
+  end
+
+  let(:chef_node) { n = Chef::Node.new; n.name "testing.example.com"; n }
+  let(:node) { NodeObject.new(chef_node) }
+
+  describe "GET index" do
+    it "is successful" do
+      NodeObject.stubs(:all).returns([node])
+
+      with_constant("CHEF_ONLINE", true) do
+        get :index
+        response.should be_success
+      end
+    end
+  end
+end

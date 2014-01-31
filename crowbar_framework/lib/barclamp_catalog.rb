f9318569c9067d810b94bc0c4625cd3015f2d1f2
Balazs Kutil <bkutil@suse.com>
2014-01-31 17:07:27 +0100

    Add shortcut method for catalog['barclamps']

--- crowbar_framework/lib/barclamp_catalog.rb
+++ crowbar_framework/lib/barclamp_catalog.rb
@@ -18,32 +18,36 @@ class BarclampCatalog
     BarclampCatalog.instance.send(method, *args, &block)
   end
 
+  def barclamps
+    catalog['barclamps']
+  end
+
   def chef_order(barclamp)
     return DEFAULT_CHEF_ORDER if barclamp.nil?
-    order = (catalog["barclamps"][barclamp]["order"] || DEFAULT_CHEF_ORDER) rescue DEFAULT_CHEF_ORDER
-    (catalog["barclamps"][barclamp]["chef_order"] || order) rescue order
+    order = (barclamps[barclamp]["order"] || DEFAULT_CHEF_ORDER) rescue DEFAULT_CHEF_ORDER
+    (barclamps[barclamp]["chef_order"] || order) rescue order
   end
 
   def run_order(barclamp)
     return DEFAULT_RUN_ORDER if barclamp.nil?
-    order = (catalog["barclamps"][barclamp]["order"] || DEFAULT_RUN_ORDER) rescue DEFAULT_RUN_ORDER
-    (catalog["barclamps"][barclamp]["run_order"] || order) rescue order
+    order = (barclamps[barclamp]["order"] || DEFAULT_RUN_ORDER) rescue DEFAULT_RUN_ORDER
+    (barclamps[barclamp]["run_order"] || order) rescue order
   end
 
   def members(barclamp)
-    catalog["barclamps"][barclamp].nil? ? [] : catalog["barclamps"][barclamp]['members']
+    barclamps[barclamp].nil? ? [] : barclamps[barclamp]['members']
   end
 
   def category(barclamp)
     value = @categories.map do |parent, members|
       next unless members.include? barclamp
-      catalog["barclamps"][parent]["display"]
+      barclamps[parent]["display"]
     end
     value.compact.first || DEFAULT_CATEGORY
   end
 
   def display_name(barclamp)
-    display = catalog['barclamps'][barclamp]['display']
+    display = barclamps[barclamp]['display']
 
     if display.nil? or display.empty?
       barclamp.titlecase
@@ -56,7 +60,7 @@ class BarclampCatalog
 
   def build_categories
     {}.tap do |result|
-      catalog["barclamps"].each do |barclamp, attrs|
+      barclamps.each do |barclamp, attrs|
         next if attrs["members"].nil?
         result[barclamp] = attrs["members"].keys
       end

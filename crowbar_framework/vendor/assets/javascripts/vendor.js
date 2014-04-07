92283c9e8b7c1f5bc9632e6a0a3bd1f02ecfff44
Thomas Boerger <tboerger@suse.de>
2014-04-07 16:22:00 +0200

    Moved javascripts into vendor folder

---
+++ crowbar_framework/vendor/assets/javascripts/vendor.js
@@ -0,0 +1,48 @@
+//= require jquery/jquery
+//= require jquery/ui
+//= require jquery/rails
+//= require jquery/blockui
+//= require jquery/hideShowPassword
+//= require jquery/sparkline
+//= require jquery/spritely
+//= require jquery/ledUpdate
+//= require jquery/queryString
+//= require jquery/listSearch
+//= require jquery/nodeList
+//= require jquery/jsonAttribute
+//= require jquery/updateAttribute
+//= require jquery/messages
+//= require jquery/dynamicTable
+
+//= require bootstrap/affix
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/carousel
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+//= require bootstrap/tab
+//= require bootstrap/transition
+//= require bootstrap/scrollspy
+//= require bootstrap/modal
+//= require bootstrap/tooltip
+//= require bootstrap/popover
+//= require bootstrap/hover-dropdown
+//= require bootstrap/select
+//= require bootstrap/growl
+
+//= require angular/angular
+//= require angular/touch
+//= require angular/resource
+
+//= require codemirror/codemirror
+//= require codemirror/javascript
+
+//= require misc/sprintf
+//= require misc/format
+//= require misc/arrayRemove
+//= require misc/stringLocalize
+//= require misc/localizedValue
+//= require misc/iefix
+//= require misc/handlebars
+
+//= require faye

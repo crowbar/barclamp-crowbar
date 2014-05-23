7a94fde1ac3a8f5b46ce6496830d6597212c8484
Balazs Kutil <bkutil@suse.com>
2014-05-23 10:37:56 +0200

    Move role revision updates to a separate method

--- crowbar_framework/app/models/role_object.rb
+++ crowbar_framework/app/models/role_object.rb
@@ -168,36 +168,43 @@ class RoleObject < ChefObject
     @role = x
   end
 
-  def save
-    @role.override_attributes[barclamp] = {} if @role.override_attributes[barclamp].nil?
-    if @role.override_attributes[barclamp]["crowbar-revision"].nil?
-      @role.override_attributes[barclamp]["crowbar-revision"] = 0
+  def crowbar_revision
+    override_attributes[barclamp] ? override_attributes[barclamp]["crowbar-revision"].to_i : 0
+  end
+
+  def increment_crowbar_revision!
+    override_attributes[barclamp] ||= {}
+    if override_attributes[barclamp]["crowbar-revision"].nil?
+      override_attributes[barclamp]["crowbar-revision"] = 0
     else
-      @role.override_attributes[barclamp]["crowbar-revision"] = @role.override_attributes[barclamp]["crowbar-revision"] + 1
+      override_attributes[barclamp]["crowbar-revision"] += 1
     end
-    Rails.logger.debug("Saving role: #{@role.name} - #{@role.override_attributes[barclamp]["crowbar-revision"]}")
+  end
+
+  def save
+    Rails.logger.debug("Saving role: #{@role.name} - #{crowbar_revision}")
     role_lock = FileLock.acquire "role:#{@role.name}"
     begin
       old_role = RoleObject.find_role_by_name(@role.name)
       if old_role
-        old_role.override_attributes[barclamp] ||= {}
-        old_rev = old_role.override_attributes[barclamp]["crowbar-revision"]
-        new_rev = @role.override_attributes[barclamp]["crowbar-revision"]
+        old_rev = old_role.crowbar_revision
+        new_rev = crowbar_revision
         if old_rev && old_rev >= new_rev
           Rails.logger.warn("WARNING: revision race for role #{@role.name} (previous revision #{old_rev})")
         end
       end
+      increment_crowbar_revision!
       @role.save
     ensure
       FileLock.release role_lock
     end
-    Rails.logger.debug("Done saving role: #{@role.name} - #{@role.override_attributes[barclamp]["crowbar-revision"]}")
+    Rails.logger.debug("Done saving role: #{@role.name} - #{crowbar_revision}")
   end
 
   def destroy
-    Rails.logger.debug("Destroying role: #{@role.name} - #{@role.override_attributes[barclamp]["crowbar-revision"]}")
+    Rails.logger.debug("Destroying role: #{@role.name} - #{crowbar_revision}")
     @role.destroy
-    Rails.logger.debug("Done removing role: #{@role.name} - #{@role.override_attributes[barclamp]["crowbar-revision"]}")
+    Rails.logger.debug("Done removing role: #{@role.name} - #{crowbar_revision}")
   end
 
   def elements

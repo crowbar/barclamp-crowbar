crowbar/barclamp-crowbar

commit ff7e15ae365a685d2f19a828d744524b7518db0c

Remove references to CHEF_ONLINE from the code
This constant was used previously for testing. However, we currently use
OfflineChef (spec/support/offline_chef.rb) to stub chef responses, so
this is no longer needed.

This patch takes care of removing the code dependent on this constant
being false, i.e. we assume that CHEF_ONLINE is always true. This
simplifies code logic on a lot of places.

crowbar_framework/app/models/service_object.rb
@@ -24,7 +24,6 @@ require 'hash_only_merge'
 
 class ServiceObject
   FORBIDDEN_PROPOSAL_NAMES=["template","nodes","commit","status"]
-  extend CrowbarOffline
 
   attr_accessor :bc_name
 
@@ -780,11 +779,7 @@ class ServiceObject
   end
 
   def proposal_schema_directory
-    if CHEF_ONLINE
-      Rails.root.join("..", "chef", "data_bags", "crowbar").expand_path
-    else
-      Rails.root.join("schema")
-    end
+    Rails.root.join("..", "chef", "data_bags", "crowbar").expand_path
   end
 
   #
@@ -1126,92 +1121,89 @@ class ServiceObject
       @logger.debug("AR: Calling chef-client for #{role.name} on non-admin nodes #{non_admin_nodes.join(" ")}")
       @logger.debug("AR: Calling chef-client for #{role.name} on admin nodes #{admin_list.join(" ")}")
 
-      # Only take the actions if we are online
-      if CHEF_ONLINE
-        #
-        # XXX: We used to do this twice - do we really need twice???
-        # Yes! We do!  The system has some transient issues that are hidden
-        # but the double run for failing nodes.  For now, we will do this.
-        # Make this better one day.
-        #
-        pids = {}
-        unless non_admin_nodes.empty?
-          non_admin_nodes.each do |node|
-            nobj = NodeObject.find_node_by_name(node)
-            unless nobj[:platform] == "windows"
+      #
+      # XXX: We used to do this twice - do we really need twice???
+      # Yes! We do!  The system has some transient issues that are hidden
+      # but the double run for failing nodes.  For now, we will do this.
+      # Make this better one day.
+      #
+      pids = {}
+      unless non_admin_nodes.empty?
+        non_admin_nodes.each do |node|
+          nobj = NodeObject.find_node_by_name(node)
+          unless nobj[:platform] == "windows"
+            filename = "#{CROWBAR_LOG_DIR}/chef-client/#{node}.log"
+            pid = run_remote_chef_client(node, "chef-client", filename)
+            pids[pid] = node
+          end
+        end
+        status = Process.waitall
+        badones = status.select { |x| x[1].exitstatus != 0 }
+
+        unless badones.empty?
+          unless oneshot?
+            badones.each do |baddie|
+              node = pids[baddie[0]]
+              @logger.warn("Re-running chef-client again for a failure: #{node} #{@bc_name} #{inst}")
               filename = "#{CROWBAR_LOG_DIR}/chef-client/#{node}.log"
               pid = run_remote_chef_client(node, "chef-client", filename)
               pids[pid] = node
             end
+            status = Process.waitall
+            badones = status.select { |x| x[1].exitstatus != 0 }
           end
-          status = Process.waitall
-          badones = status.select { |x| x[1].exitstatus != 0 }
 
           unless badones.empty?
-            unless oneshot?
-              badones.each do |baddie|
-                node = pids[baddie[0]]
-                @logger.warn("Re-running chef-client again for a failure: #{node} #{@bc_name} #{inst}")
-                filename = "#{CROWBAR_LOG_DIR}/chef-client/#{node}.log"
-                pid = run_remote_chef_client(node, "chef-client", filename)
-                pids[pid] = node
-              end
-              status = Process.waitall
-              badones = status.select { |x| x[1].exitstatus != 0 }
+            message = "Failed to apply the proposal to: "
+            badones.each do |baddie|
+              message = message + "#{pids[baddie[0]]} \n"+ get_log_lines("#{pids[baddie[0]]}")
             end
-
-            unless badones.empty?
-              message = "Failed to apply the proposal to: "
-              badones.each do |baddie|
-                message = message + "#{pids[baddie[0]]} \n"+ get_log_lines("#{pids[baddie[0]]}")
-              end
-              update_proposal_status(inst, "failed", message)
-              restore_to_ready(all_nodes)
-              process_queue unless in_queue
-              return [ 405, message ]
-            end
+            update_proposal_status(inst, "failed", message)
+            restore_to_ready(all_nodes)
+            process_queue unless in_queue
+            return [ 405, message ]
           end
         end
+      end
 
-        unless admin_list.empty?
-          admin_list.each do |node|
-            filename = "#{CROWBAR_LOG_DIR}/chef-client/#{node}.log"
-            pid = run_remote_chef_client(node, Rails.root.join("..", "bin", "single_chef_client.sh").expand_path, filename)
-            pids[node] = pid
+      unless admin_list.empty?
+        admin_list.each do |node|
+          filename = "#{CROWBAR_LOG_DIR}/chef-client/#{node}.log"
+          pid = run_remote_chef_client(node, Rails.root.join("..", "bin", "single_chef_client.sh").expand_path, filename)
+          pids[node] = pid
+        end
+        status = Process.waitall
+        badones = status.select { |x| x[1].exitstatus != 0 }
+
+        unless badones.empty?
+          unless oneshot?
+            badones.each do |baddie|
+              node = pids[baddie[0]]
+              @logger.warn("Re-running chef-client (admin) again for a failure: #{node} #{@bc_name} #{inst}")
+              filename = "#{CROWBAR_LOG_DIR}/chef-client/#{node}.log"
+              pid = run_remote_chef_client(node, Rails.root.join("..", "bin", "single_chef_client.sh").expand_path, filename)
+              pids[pid] = node
+            end
+            status = Process.waitall
+            badones = status.select { |x| x[1].exitstatus != 0 }
           end
-          status = Process.waitall
-          badones = status.select { |x| x[1].exitstatus != 0 }
 
           unless badones.empty?
-            unless oneshot?
-              badones.each do |baddie|
-                node = pids[baddie[0]]
-                @logger.warn("Re-running chef-client (admin) again for a failure: #{node} #{@bc_name} #{inst}")
-                filename = "#{CROWBAR_LOG_DIR}/chef-client/#{node}.log"
-                pid = run_remote_chef_client(node, Rails.root.join("..", "bin", "single_chef_client.sh").expand_path, filename)
-                pids[pid] = node
-              end
-              status = Process.waitall
-              badones = status.select { |x| x[1].exitstatus != 0 }
+            message = "Failed to apply the proposal to: "
+            badones.each do |baddie|
+              message = message + "#{pids[baddie[0]]} \n "+ get_log_lines("#{pids[baddie[0]]}")
             end
-
-            unless badones.empty?
-              message = "Failed to apply the proposal to: "
-              badones.each do |baddie|
-                message = message + "#{pids[baddie[0]]} \n "+ get_log_lines("#{pids[baddie[0]]}")
-              end
-              update_proposal_status(inst, "failed", message)
-              restore_to_ready(all_nodes)
-              process_queue unless in_queue
-              return [ 405, message ]
-            end
+            update_proposal_status(inst, "failed", message)
+            restore_to_ready(all_nodes)
+            process_queue unless in_queue
+            return [ 405, message ]
           end
         end
       end
     end
 
     # XXX: This should not be done this way.  Something else should request this.
-    system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path) if CHEF_ONLINE and !ran_admin
+    system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path) and !ran_admin
 
     begin
       apply_role_post_chef_call(old_role, role, all_nodes)

crowbar_framework/app/controllers/support_controller.rb
@@ -125,30 +125,26 @@ class SupportController < ApplicationController
   end
 
   def export_chef
-    if CHEF_ONLINE
-      begin
-        Rails.root.join("db").children.each do |file|
-          file.unlink if file.extname == ".json"
-        end
-
-        NodeObject.all.each { |n| n.export }
-        RoleObject.all.each { |r| r.export }
-        ProposalObject.all.each { |p| p.export }
-
-        filename = "crowbar-chef-#{Time.now.strftime("%Y%m%d-%H%M%S")}.tgz"
-
-        pid = fork do
-          system "tar", "-czf", Rails.root.join("tmp", filename), Rails.root.join("db", "*.json").to_s
-          File.rename Rails.root.join("tmp", filename), export_dir.join(filename)
-        end
-
-        Process.detach(pid)
-        redirect_to utils_url(:waiting => true, :file => filename) and return
-      rescue StandardError => e
-        flash[:alert] = I18n.t("support.export.fail", :error => e.message)
+    begin
+      Rails.root.join("db").children.each do |file|
+        file.unlink if file.extname == ".json"
       end
-    else
-      flash[:alert] = t("support.index.offline_mode")
+
+      NodeObject.all.each { |n| n.export }
+      RoleObject.all.each { |r| r.export }
+      ProposalObject.all.each { |p| p.export }
+
+      filename = "crowbar-chef-#{Time.now.strftime("%Y%m%d-%H%M%S")}.tgz"
+
+      pid = fork do
+        system "tar", "-czf", Rails.root.join("tmp", filename), Rails.root.join("db", "*.json").to_s
+        File.rename Rails.root.join("tmp", filename), export_dir.join(filename)
+      end
+
+      Process.detach(pid)
+      redirect_to utils_url(:waiting => true, :file => filename) and return
+    rescue StandardError => e
+      flash[:alert] = I18n.t("support.export.fail", :error => e.message)
     end
 
     redirect_to utils_url

crowbar_framework/app/models/chef_object.rb
@@ -18,7 +18,6 @@
 #
 
 class ChefObject
-  extend CrowbarOffline
   @@CrowbarDomain = nil
   
   NOT_SET = 'not_set'
@@ -28,12 +27,12 @@ class ChefObject
       # NOTE: We are using a global here to avoid lookups.  We need to consider some better cache/expiration strategy
       if @@CrowbarDomain.nil?
         bag = ProposalObject.find_proposal('dns', 'default')
-        @@CrowbarDomain = bag[:attributes][:dns][:domain] || (CHEF_ONLINE ? %x{dnsdomainname}.strip : OFFLINE_DOMAIN)
+        @@CrowbarDomain = bag[:attributes][:dns][:domain] || %x{dnsdomainname}.strip
       end
       return @@CrowbarDomain
     rescue StandardError => e
       Rails.logger.warn("Could not lookup domain name from Crowbar DNS barclamp attributes/dns/domain key.  Error #{e.message}.")
-      @@CrowbarDomain = (CHEF_ONLINE ? nil : OFFLINE_DOMAIN) # reset to make sure we do not cache it
+      @@CrowbarDomain = nil # reset to make sure we do not cache it
       return %x{dnsdomainname}.strip
     end
   end

crowbar_framework/app/models/node_object.rb
@@ -21,26 +21,19 @@ require 'chef/mixin/deep_merge'
 require 'timeout'
 
 class NodeObject < ChefObject
-  extend CrowbarOffline
-
   def self.find(search)
     answer = []
-    if CHEF_ONLINE
-      nodes = if search.nil?
-        ChefObject.query_chef.search "node"
-      else
-        ChefObject.query_chef.search "node", "#{chef_escape(search)}"
-      end
-      if nodes[2] != 0 and !nodes[0].nil?
-        nodes[0].delete_if { |x| x.nil? }
-        answer = nodes[0].map do |x|
-          NodeObject.new x
-        end
-        answer.delete_if { |x| !x.has_chef_server_roles? }
-      end
+    nodes = if search.nil?
+      ChefObject.query_chef.search "node"
     else
-      files = offline_search 'node-', (search.nil? ? '' : search[5..100])
-      answer = files.map! { |f| NodeObject.new(recover_json(f)) }
+      ChefObject.query_chef.search "node", "#{chef_escape(search)}"
+    end
+    if nodes[2] != 0 and !nodes[0].nil?
+      nodes[0].delete_if { |x| x.nil? }
+      answer = nodes[0].map do |x|
+        NodeObject.new x
+      end
+      answer.delete_if { |x| !x.has_chef_server_roles? }
     end
     return answer
   end
@@ -69,11 +62,7 @@ class NodeObject < ChefObject
   end
 
   def self.find_node_by_public_name(name)
-    nodes = if CHEF_ONLINE
-      self.find "crowbar_public_name:#{chef_escape(name)}"
-    else
-      nodes = self.find_all_nodes.keep_if { |n| n.public_name==name }
-    end
+    nodes = self.find "crowbar_public_name:#{chef_escape(name)}"
     if nodes.length == 1
       return nodes[0]
     elsif nodes.length == 0
@@ -84,13 +73,8 @@ class NodeObject < ChefObject
   end
 
   def self.find_node_by_name(name)
-    val = if CHEF_ONLINE
-      name += ".#{ChefObject.cloud_domain}" unless name =~ /(.*)\.(.)/
-      ChefObject.crowbar_node(name)
-    else
-      name += ".#{OFFLINE_DOMAIN || ChefObject.cloud_domain}" unless name =~ /(.*)\.(.)/
-      self.recover_json(self.nfile('node',name))
-    end
+    name += ".#{ChefObject.cloud_domain}" unless name =~ /(.*)\.(.)/
+    val = ChefObject.crowbar_node(name)
     return val.nil? ? nil : NodeObject.new(val)
   end
 
@@ -114,12 +98,7 @@ class NodeObject < ChefObject
 
   def self.create_new_role(new_name, machine)
     name = make_role_name new_name
-    if CHEF_ONLINE
-      role = RoleObject.new Chef::Role.new
-    else
-      self.create_object 'role', name, "NodeObject Create New Role"
-      role = RoleObject.find_role_by_name(name)
-    end
+    role = RoleObject.new Chef::Role.new
     role.name = name
     role.default_attributes["crowbar"] = {}
     role.default_attributes["crowbar"]["network"] = {} if role.default_attributes["crowbar"]["network"].nil?
@@ -155,7 +134,6 @@ class NodeObject < ChefObject
         @role = NodeObject.create_new_role(node.name, node)
       else
         Rails.logger.fatal("Node exists without role!! #{node.name}")
-        NodeObject.offline_remove('node', node.name) if !CHEF_ONLINE
       end
     end
     # deep clone of @role.default_attributes, used when saving node
@@ -251,7 +229,7 @@ class NodeObject < ChefObject
         set_display "alias", value
         @role.description = chef_description
         # move this to event driven model one day
-        system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path) if CHEF_ONLINE
+        system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path)
       end
     end
     return value
@@ -331,7 +309,7 @@ class NodeObject < ChefObject
 
   def state
     return 'unknown' if (@node.nil? or @role.nil?)
-    if self.crowbar['state'] === 'ready' and CHEF_ONLINE and @node['ohai_time']
+    if self.crowbar['state'] === 'ready' and @node['ohai_time']
       since_last = Time.now.to_i-@node['ohai_time'].to_i
       return 'noupdate' if since_last > 1200 # or 20 mins
     end
@@ -576,13 +554,8 @@ class NodeObject < ChefObject
     _remove_elements_from_node(@attrs_last_saved, @role.default_attributes, @node.normal_attrs)
     Chef::Mixin::DeepMerge::deep_merge!(@role.default_attributes, @node.normal_attrs, {})
 
-    if CHEF_ONLINE
-      @role.save
-      @node.save
-    else
-      NodeObject.offline_cache(@node, NodeObject.nfile('node', @node.name))
-      NodeObject.offline_cache(@role, RoleObject.nfile('role', @role.name))
-    end
+    @role.save
+    @node.save
 
     # update deep clone of @role.default_attributes
     @attrs_last_saved = deep_clone(@role.default_attributes)
@@ -982,15 +955,9 @@ class NodeObject < ChefObject
   end
 
   def set_state(state)
-    if CHEF_ONLINE
-      # use the real transition function for this
-      cb = CrowbarService.new Rails.logger
-      results = cb.transition "default", @node.name, state
-    else
-      puts "Node #{name} Chef State Changed to #{state}"
-      self.crowbar['state'] = state
-      save
-    end
+    # use the real transition function for this
+    cb = CrowbarService.new Rails.logger
+    results = cb.transition "default", @node.name, state
 
     if %w(reset reinstall update).include? state
       # wait with reboot for the finish of configuration update by local chef-client
@@ -1006,12 +973,7 @@ class NodeObject < ChefObject
         Rails.logger.warn("chef client seems to be still running after 5 minutes of wait; going on with the reboot")
       end
 
-      if CHEF_ONLINE
-        bmc_cmd("power cycle")
-      else
-        NodeObject.clear_cache @node
-        puts "Node #{name} to #{state} caused cache object to be deleted."
-      end
+      bmc_cmd("power cycle")
     end
     results
   end
@@ -1034,24 +996,20 @@ class NodeObject < ChefObject
 
   def reboot
     set_state("reboot")
-    return puts "Node #{name} IMPI Reboot call to #{bmc_address}" unless CHEF_ONLINE
     bmc_cmd("power cycle")
   end
 
   def shutdown
     set_state("shutdown")
-    return puts "Node #{name} IMPI Shutdown call to #{bmc_address}" unless CHEF_ONLINE
     bmc_cmd("power off")
   end
 
   def poweron
     set_state("poweron")
-    return puts "Node #{name} IMPI Power On call to #{bmc_address}" unless CHEF_ONLINE
     bmc_cmd("power on")
   end
 
   def identify
-    return puts "Node #{name} IMPI Identify call to #{bmc_address}" unless CHEF_ONLINE
     bmc_cmd("chassis identify")
   end
 

crowbar_framework/app/models/proposal_object.rb
@@ -18,10 +18,8 @@
 #
 
 class ProposalObject < ChefObject
-
-  extend CrowbarOffline
   BC_PREFIX = 'bc-template-'
-  
+
   def self.find_data_bag_item(bag)
     begin
       bag = ProposalObject.new(Chef::DataBag.load bag)  #should use new syntax
@@ -34,17 +32,12 @@ class ProposalObject < ChefObject
   def self.find(search)
     props = [] 
     begin
-      if CHEF_ONLINE
-        arr = ChefObject.query_chef.search("crowbar", "id:#{chef_escape(search)}") 
-        if arr[2] != 0
-          props = arr[0].map do |x| 
-            ProposalObject.new x 
-          end
-          props.delete_if { |x| x.nil? or x.item.nil? }
+      arr = ChefObject.query_chef.search("crowbar", "id:#{chef_escape(search)}") 
+      if arr[2] != 0
+        props = arr[0].map do |x| 
+          ProposalObject.new x 
         end
-      else
-        files = self.offline_search('data_bag_item_crowbar-', search.chop)
-        props = files.map { |file| ProposalObject.new(self.recover_json(file)) }
+        props.delete_if { |x| x.nil? or x.item.nil? }
       end
     rescue StandardError => e
        Rails.logger.error("Could not recover Chef Crowbar data searching for '#{search}' due to '#{e.inspect}'")
@@ -69,11 +62,7 @@ class ProposalObject < ChefObject
   end
 
   def self.find_proposal_by_id(id)
-    val = if CHEF_ONLINE
-      ChefObject.crowbar_data(id)
-    else
-      self.recover_json(self.nfile('data_bag_item_crowbar-bc', id[/bc-(.*)/,1]))
-    end
+    val = ChefObject.crowbar_data(id)
     return val.nil? ? nil : ProposalObject.new(val)
   end
 
@@ -251,11 +240,7 @@ class ProposalObject < ChefObject
       @item["deployment"][barclamp]["crowbar-revision"] = @item["deployment"][barclamp]["crowbar-revision"] + 1
     end
     Rails.logger.debug("Saving data bag item: #{@item["id"]} - #{@item["deployment"][barclamp]["crowbar-revision"]}")
-    if CHEF_ONLINE
-      @item.save
-    else
-      ProposalObject.offline_cache(@item, ProposalObject.nfile('data_bag_item_crowbar', @item.id))
-    end
+    @item.save
     Rails.logger.debug("Done saving data bag item: #{@item["id"]} - #{@item["deployment"][barclamp]["crowbar-revision"]}")
   end
 

crowbar_framework/app/models/role_object.rb
@@ -18,8 +18,6 @@
 #
 
 class RoleObject < ChefObject
-  extend CrowbarOffline
-
   def self.all
     self.find_roles_by_search(nil)
   end
@@ -35,49 +33,35 @@ class RoleObject < ChefObject
   
   def self.find_roles_by_name(name)
     roles = []
-    if CHEF_ONLINE
-      #TODO this call could be moved to fild_roles_by_search
-      arr = ChefObject.query_chef.search "role", "name:#{chef_escape(name)}"
-      if arr[2] != 0
-        roles = arr[0].map { |x| RoleObject.new x }
-        roles.delete_if { |x| x.nil? or x.role.nil? }
-      end
-    else
-      roles = find_roles_by_search name
+    #TODO this call could be moved to fild_roles_by_search
+    arr = ChefObject.query_chef.search "role", "name:#{chef_escape(name)}"
+    if arr[2] != 0
+      roles = arr[0].map { |x| RoleObject.new x }
+      roles.delete_if { |x| x.nil? or x.role.nil? }
     end
     roles
   end
 
   def self.find_roles_by_search(search)
     roles = []
-    if CHEF_ONLINE
-      arr = if search.nil?
-        ChefObject.query_chef.search "role"
-      else
-        ChefObject.query_chef.search "role", search
-      end
-      if arr[2] != 0
-        roles = arr[0].map { |x| RoleObject.new x }
-        roles.delete_if { |x| x.nil? or x.role.nil? }
-      end
+    arr = if search.nil?
+      ChefObject.query_chef.search "role"
     else
-      files = offline_search 'role-', search
-      roles = files.map! { |f| RoleObject.new(recover_json(f)) }
+      ChefObject.query_chef.search "role", search
+    end
+    if arr[2] != 0
+      roles = arr[0].map { |x| RoleObject.new x }
+      roles.delete_if { |x| x.nil? or x.role.nil? }
     end
     roles
   end
 
   def self.find_role_by_name(name)
-    if CHEF_ONLINE
-      begin
-        return RoleObject.new Chef::Role.load(name)
-      rescue Net::HTTPServerException => e
-        return nil if e.response.code == "404"
-        raise e
-      end
-    else
-      answer = self.recover_json(self.nfile('role',name))
-      return answer.nil? ? nil : RoleObject.new(answer)
+    begin
+      return RoleObject.new Chef::Role.load(name)
+    rescue Net::HTTPServerException => e
+      return nil if e.response.code == "404"
+      raise e
     end
   end
 
@@ -216,20 +200,16 @@ class RoleObject < ChefObject
     Rails.logger.debug("Saving role: #{@role.name} - #{@role.override_attributes[barclamp]["crowbar-revision"]}")
     role_lock = acquire_lock "role:#{@role.name}"
     begin
-      if CHEF_ONLINE
-        old_role = RoleObject.find_role_by_name(@role.name)
-        if old_role
-          old_role.override_attributes[barclamp] ||= {}
-          old_rev = old_role.override_attributes[barclamp]["crowbar-revision"]
-          new_rev = @role.override_attributes[barclamp]["crowbar-revision"]
-          if old_rev && old_rev >= new_rev
-            Rails.logger.warn("WARNING: revision race for role #{@role.name} (previous revision #{old_rev})")
-          end
+      old_role = RoleObject.find_role_by_name(@role.name)
+      if old_role
+        old_role.override_attributes[barclamp] ||= {}
+        old_rev = old_role.override_attributes[barclamp]["crowbar-revision"]
+        new_rev = @role.override_attributes[barclamp]["crowbar-revision"]
+        if old_rev && old_rev >= new_rev
+          Rails.logger.warn("WARNING: revision race for role #{@role.name} (previous revision #{old_rev})")
         end
-        @role.save
-      else
-        RoleObject.offline_cache(@role, RoleObject.nfile('role', @role.name))
       end
+      @role.save
     ensure
       release_lock role_lock
     end

crowbar_framework/app/views/shared/_flashes.html.haml
@@ -1,7 +1,3 @@
-- unless CHEF_ONLINE
-  .alert.alert-danger
-    = t("offline")
-
 - flash.each do |key, message|
   .alert.alert-dismissable{ :class => "alert-#{flash_for key}"}
     %button.close{ :type => "button", "data-dismiss" => "alert", "aria-hidden" => true }

crowbar_framework/config/environment.rb
@@ -41,8 +41,6 @@ Rails::Initializer.run do |config|
   CHEF_CLIENT_KEY = "/opt/dell/crowbar_framework/config/client.pem" unless defined? CHEF_CLIENT_KEY
   CHEF_NODE_NAME ="crowbar" unless defined? CHEF_NODE_NAME
   CHEF_SERVER_URL = "http://localhost:4000" unless defined? CHEF_SERVER_URL
-  CHEF_ONLINE = true unless defined? CHEF_ONLINE
-  OFFLINE_FILES_DIR = "db" unless defined? OFFLINE_FILES_DIR
   CROWBAR_VERSION = "0.0.1" unless defined? CROWBAR_VERSION
 
   CONVERGED_ADMIN = true # flag indicating at we can assume all Crowbar services on a single server

commit c658f6878f28b83fb35a203c8a49bf95c2f02def

Fix typo that crept in after removing CHEF_ONLINE
Introduced in ff7e15ae

crowbar_framework/app/models/service_object.rb
@@ -1323,7 +1323,7 @@ class ServiceObject
     end
 
     # XXX: This should not be done this way.  Something else should request this.
-    system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path) and !ran_admin
+    system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path) if !ran_admin
 
     begin
       apply_role_post_chef_call(old_role, role, all_nodes)
d293675d98301daa0e6e3ea426022591af48b56e
Vincent Untz <vuntz@suse.com>
2014-07-24 14:26:37 +0200

    Add FIXME about barclamp-crowbar having to know about other barclamps

--- crowbar_framework/app/models/role_object.rb
+++ crowbar_framework/app/models/role_object.rb
@@ -86,6 +86,8 @@ class RoleObject < ChefObject
   end
 
   def barclamp
+    # FIXME: this obviously shouldn't need to exist; we need a proper registry
+    # to avoid barclamp-crowbar having to know about other barclamps...
     name = @role.name.split("-")[0]
 
     case name

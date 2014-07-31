crowbar/barclamp-crowbar

commit 5614efdcf9250cf4e0dc92b2e44d4b17b33401d2

Fix barclamp and name methods to work w/ templates
```barclamp``` method, when called on a barclamp template, returned
'template'. Similarly, ```name``` returned the barclamp name.

This then broke the all_elements method and possibly others.

crowbar_framework/app/models/proposal_object.rb
@@ -129,13 +129,15 @@ class ProposalObject < ChefObject
   def id
     @item['id']
   end
-  
+
   def name
-    @item.name[/crowbar_bc-(.*)-(.*)$/,2]
+    match = @item.name.match(/crowbar_bc-(.*)-(.*)$/)
+    match[1] == 'template' ? match[1] : match[2]
   end
-  
+
   def barclamp
-    @item.name[/crowbar_bc-(.*)-(.*)$/,1]
+    match = @item.name.match(/crowbar_bc-(.*)-(.*)$/)
+    match[1] == 'template' ? match[2] : match[1]
   end
 
   def prop

crowbar_framework/spec/models/proposal_object_spec.rb
@@ -1,6 +1,30 @@
 require 'spec_helper'
 
 describe ProposalObject do
+  describe "barclamp" do
+    it "returns barclamp for regular proposals" do
+      proposal = ProposalObject.find_proposal_by_id("bc-crowbar-default")
+      proposal.barclamp.should == 'crowbar'
+    end
+
+    it "returns barclamp for templates" do
+      proposal = ProposalObject.find_barclamp("crowbar")
+      proposal.barclamp.should == 'crowbar'
+    end
+  end
+
+  describe "name" do
+    it "returns name for regular proposals" do
+      proposal = ProposalObject.find_proposal_by_id("bc-crowbar-default")
+      proposal.name.should == 'default'
+    end
+
+    it "returns name for templates" do
+      proposal = ProposalObject.find_barclamp("crowbar")
+      proposal.name.should == 'template'
+    end
+  end
+
   describe "finders" do
     describe "find" do
       it "returns proposals matching a search" do

commit 4a290facb7b84cdf9eaff480f2c1617a8bfd02b9

Fix barclamp and name methods to work w/ networks
ProposalObject is (ab)used to also write data bags that contain network
information (and not proposals).

Commit 5614efd broke this because the regular expressions were not
matching at all.

This is only really visible when the network barclamp is applied, which
is usually on install.

crowbar_framework/app/models/proposal_object.rb
@@ -137,12 +137,22 @@ class ProposalObject < ChefObject
 
   def name
     match = @item.name.match(/crowbar_bc-(.*)-(.*)$/)
-    match[1] == 'template' ? match[1] : match[2]
+    if match.nil?
+      match = @item.name.match(/crowbar_(.*)_network$/)
+      match.nil? ? "" : match[1]
+    else
+      match[1] == 'template' ? match[1] : match[2]
+    end
   end
 
   def barclamp
     match = @item.name.match(/crowbar_bc-(.*)-(.*)$/)
-    match[1] == 'template' ? match[2] : match[1]
+    if match.nil?
+      match = @item.name.match(/crowbar_(.*)_network$/)
+      match.nil? ? "" : "network"
+    else
+      match[1] == 'template' ? match[2] : match[1]
+    end
   end
 
   def prop

crowbar_framework/spec/models/proposal_object_spec.rb
@@ -28,6 +28,11 @@ describe ProposalObject do
       proposal = ProposalObject.find_barclamp("crowbar")
       proposal.barclamp.should == 'crowbar'
     end
+
+    it "returns network for networks" do
+      proposal = ProposalObject.find_proposal_by_id("admin_network")
+      proposal.barclamp.should == 'network'
+    end
   end
 
   describe "name" do
@@ -39,6 +44,11 @@ describe ProposalObject do
     it "returns name for templates" do
       proposal = ProposalObject.find_barclamp("crowbar")
       proposal.name.should == 'template'
+    end
+
+    it "returns name for networks" do
+      proposal = ProposalObject.find_barclamp("admin_network")
+      proposal.name.should == 'admin'
     end
   end
 
d4809f2d79bd881cbd35d7aeb33967477b5ec7f3
Ionuț Arțăriși <iartarisi@suse.cz>
2014-09-10 12:07:26 +0200

    cleanup unused variables
    - turn is_admin into a helper and use that instead
    - level :info is the default log level

--- chef/cookbooks/utils/libraries/helpers.rb
+++ chef/cookbooks/utils/libraries/helpers.rb
@@ -1,3 +1,18 @@
+# Copyright (c) 2014 SUSE Linux GmbH.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 module CrowbarHelper
   def self.get_host_for_admin_url(node, use_cluster = false)
     if use_cluster && defined?(CrowbarPacemakerHelper)
@@ -36,5 +51,11 @@ module CrowbarHelper
     end
 
     public_name
+  end
+
+  def self.is_admin?(node)
+    # XXX Node::Attribute.fetch doesn't work with a 'false' value in
+    # chef10 so we have to rescue false (see e.g. CHEF-3736)
+    !!node["crowbar"].fetch("admin_node", false) rescue false
   end
 end

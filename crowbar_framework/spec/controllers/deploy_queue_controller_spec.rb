d43ba81cba14ba01924b1c218a1e4b71b290a1cb
Balazs Kutil <balazs@kutilovi.cz>
2015-02-19 10:35:29 +0100

    Support for clusters in the queue
    The code is unfortunately quite hairy for following reasons:
    
    1. Cluster is just a string tag, not a separate class, or a model,
    associated with nodes. So it is not possible to easily retrieve the
    actual node objects, or their status, to get the status of the whole
    cluster. This is visible in deploy_queue/_missing_nodes.html.haml, where
    the expansion and mapping has to be done manually.
    
    2. There aren't actual associations between objects, so it is neccessary
    to query for all the objects in a Chef db, then iterate and figure out
    the associations from other records.

--- crowbar_framework/spec/controllers/deploy_queue_controller_spec.rb
+++ crowbar_framework/spec/controllers/deploy_queue_controller_spec.rb
@@ -21,22 +21,64 @@ describe DeployQueueController do
   render_views
 
   describe "GET index" do
+    before do
+      # We don't have Pacemaker at hand and the helper returns false because of
+      # that, need to stub this
+      ServiceObject.stubs(:is_cluster?).returns(true)
+    end
+
+    let(:prop) { ProposalObject.find_proposal("database", "default") }
+
     it "is successful" do
       get :index
       response.should be_success
     end
 
-    it "is successful when there are clusters in the queue" do
-      # Proposal is in the queue
-      prop = ProposalObject.find_proposal("database", "default")
-      prop["deployment"][prop.barclamp]["crowbar-committing"] = true
+    describe "with existing nodes" do
+      before do
+        # Simulate the expansion to a node whose look up we can fake
+        ServiceObject.stubs(:expand_nodes_for_all).returns([["testing.crowbar.com"],[]])
+      end
 
-      # Appropriately structured queue item
-      queue_item = { "barclamp" => prop.barclamp, "inst" => prop.name, "elements" => prop.elements, "deps" => [] }
-      @controller.stubs(:deployment_queue).returns([queue_item])
+      it "is successful when a prop with clusters is deployed" do
+        # Is now deploying
+        @controller.stubs(:currently_deployed).returns(prop)
 
-      get :index
-      response.should be_success
+        get :index
+        response.should be_success
+      end
+
+      it "is successful when there are clusters in the queue" do
+        # Is queued
+        queue_item = { "barclamp" => prop.barclamp, "inst" => prop.name, "elements" => prop.elements, "deps" => [] }
+        @controller.stubs(:deployment_queue).returns([queue_item])
+
+        get :index
+        response.should be_success
+      end
+    end
+
+    describe "with non-existing nodes" do
+      before do
+        # Cluster referencing a non-existent node (deleted)
+        ServiceObject.stubs(:expand_nodes_for_all).returns([["I just dont exist"],[]])
+      end
+
+      it "is successful when a prop with clusters is deployed" do
+        # Is now deploying
+        @controller.stubs(:currently_deployed).returns(prop)
+
+        get :index
+        response.should be_success
+      end
+
+      it "is successful for clusters in the queue" do
+        queue_item = { "barclamp" => prop.barclamp, "inst" => prop.name, "elements" => prop.elements, "deps" => [] }
+        @controller.stubs(:deployment_queue).returns([queue_item])
+
+        get :index
+        response.should be_success
+      end
     end
   end
 end

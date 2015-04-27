44ecbd5958da5e4996b7651930fa58e51369c077
Balazs Kutil <bkutil@suse.com>
2015-04-27 14:27:54 +0200

    Create necessary barclamps before tests run

--- crowbar_framework/spec/controllers/nodes_controller_spec.rb
+++ crowbar_framework/spec/controllers/nodes_controller_spec.rb
@@ -21,6 +21,8 @@ describe NodesController do
   render_views
 
   before do
+    ProposalObject.any_instance.stubs(:save).returns(true)
+    Proposal.where(barclamp: "crowbar", name: "default").first_or_create(barclamp: "crowbar", name: "default")
     NodeObject.any_instance.stubs(:system).returns(true)
   end
 

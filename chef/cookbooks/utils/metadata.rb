8d22ab5db145c66ac9316069b6c3e78b48ab8c47
Thomas Bechtold <tbechtold@suse.com>
2015-07-10 09:34:03 +0200

    Update metadata for utils cookbook
    - move metadata to .rb file as recommended by chef docs
    - update maintainer contact

---
+++ chef/cookbooks/utils/metadata.rb
@@ -0,0 +1,7 @@
+name "utils"
+maintainer "Crowbar Project"
+maintainer_email "crowbar@googlegroups.com"
+license "Apache 2.0"
+description "Provides utilities for other recipes: line"
+long_description "Provides utilities for other recipes: line"
+version "0.9.5"

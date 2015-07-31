179eaf00ebbecf1f0f2b93bde9599bc23bbbd021
Balazs Kutil <balazs@kutilovi.cz>
2015-07-31 09:28:06 +0200

    Fix company name in the copyright header

--- crowbar_framework/lib/crowbar/deployment_queue.rb
+++ crowbar_framework/lib/crowbar/deployment_queue.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2015, SUSE LINUX Products GmbH
+# Copyright 2015, SUSE LINUX GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

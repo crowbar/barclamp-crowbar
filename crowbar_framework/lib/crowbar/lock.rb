18c6ec260e5fcdee6d79e76d492e773182f8d2c8
Adam Spiers <aspiers@suse.com>
2015-08-26 22:24:29 +0100

    update copyright

--- crowbar_framework/lib/crowbar/lock.rb
+++ crowbar_framework/lib/crowbar/lock.rb
@@ -1,6 +1,6 @@
 #
 # Copyright 2011-2013, Dell
-# Copyright 2013-2014, SUSE LINUX Products GmbH
+# Copyright 2013-2015, SUSE LINUX GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

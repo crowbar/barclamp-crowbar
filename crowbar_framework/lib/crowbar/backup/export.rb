db335c396ccaed2c4c127cb322096638f5117734
Maximilian Meister <mmeister@suse.de>
2016-02-18 09:08:11 +0100

    add ActiveRecord migration level to the metadata

--- crowbar_framework/lib/crowbar/backup/export.rb
+++ crowbar_framework/lib/crowbar/backup/export.rb
@@ -116,6 +116,7 @@ module Crowbar
         meta["version"] = ENV["CROWBAR_VERSION"]
         meta["created_at"] = Time.zone.now.to_s
         meta["platform"] = NodeObject.find("role:crowbar").detect(&:admin?).target_platform
+        meta["migration_level"] = ActiveRecord::Migrator.current_version
 
         workdir.join("meta.yml").open("w") do |file|
           file.write(meta.to_yaml)

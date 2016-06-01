1db4c31eb3d998226a04fb65afc65559a0bb3ec6
Nanuk Krinner <nkrinner@suse.de>
2016-06-01 11:40:37 +0200

    change call

--- crowbar_framework/config/initializers/serializer.rb
+++ crowbar_framework/config/initializers/serializer.rb
@@ -16,5 +16,5 @@
 
 # Disable root element in the JSON responses for POROs,
 # i.e. keep the behavior the same as in cloud 4
-ActiveModel::Serializer.root = false
+ActiveModelSerializers::Base.root = false
 ActiveModel::ArraySerializer.root = false
